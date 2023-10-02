9f681271133c2b469e423def8a2d2270c742b234
chimpbros <kevinnue00@gmail.com>
2023-10-02 21:02:23 +0700

    add Gameboard, GameController modules; Player and Cell factory function

--- script.js
+++ script.js
@@ -1,34 +1,124 @@
-const gameBoard = (() => {
-    const board = ['x', 'o', '', 'x', '', 'x', '', '', '']
+const GameBoard = (() => {
+    const row = 3;
+    const column = 3;
+    const board = [];
 
-    const render = () => {
-        const boardCtn = document.querySelector('#board-container');
-        boardCtn.innerHTML = '';
-        board.forEach((square, index) => {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.textContent = square;
-            cell.dataset.index = index;
-            boardCtn.appendChild(cell);
-        })
+    for(let i = 0; i < row; i++){
+        board[i] = [];
+        for(let j = 0; j < column; j++){
+            board[i].push(Cell());
+        }
+    }
+    
+    const getBoard = () => board;
+
+    const cellNumberArray = [];
+
+    let cellNumberIndex = 0;
+    for(let i = 0; i < 3; i++){
+        
+        for(let j = 0; j < 3; j++){
+            cellNumberArray[cellNumberIndex] = {};
+            cellNumberArray[cellNumberIndex].number = cellNumberIndex + 1;
+            cellNumberArray[cellNumberIndex].cell = board[i][j];
+            
+            cellNumberIndex++;
+        }
+    }
+    console.log(cellNumberArray)
+
+    const playersMove = (number, player) => {
+        const cell = cellNumberArray.filter((item) => item.number === number)[0].cell;
+        if (cell.getValue() !== 0) return;
+        cell.addMarker(player);
     }
 
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    }
+
+    
+    
     return {
-        render
-    };
+        getBoard,
+        playersMove,
+        printBoard
+    }
 })();
 
-const player = (name, char) => {
-    const sayHello = () => console.log(`my name is ${name}`);
+function Cell() {
+    let value = 0;
+
+    const addMarker = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;  
+
     return {
-        name,
-        char,
-        sayHello
+        addMarker,
+        getValue
+    }
+};
+
+
+const Player = (name, marker) => {
+    const getName = () => name;
+    const getMarker = () => marker
+
+    return {
+        getName,
+        getMarker
     };
 };
 
+const GameController = (() => {
+    let playernOneName = 'player one';
+    let playerTwoName = 'player two';
+    const board = GameBoard;
+
+    const players = [
+        {
+            name: playernOneName,
+            marker: 1
+        },
+        {
+            name: playerTwoName,
+            marker: 2
+        }
+    ];
+
+    let activePlayers = players[0];
+    const switchActivePlayers = () => {
+        activePlayers = activePlayers === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayers = () => activePlayers;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${activePlayers.name}'s turn`);
+    }
+
+    const playRound = (number) => {
+        console.log(`adding ${getActivePlayers().name}'s marker into cell ${number}`);
+
+        board.playersMove(number, getActivePlayers().marker);
+        switchActivePlayers();
+        printNewRound();
+    }
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayers
+    }
+})();
+
+const game = GameController;
+
 const startBtn = document.querySelector('#start');
 startBtn.addEventListener('click', () => {
-    gameBoard.render();
     // Game.start()
 })
